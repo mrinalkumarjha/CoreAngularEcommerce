25f936388fca3518d3a50fa73f54a2de6b1f3680
Mrinal Kumar <Mrinal.Kumar@BritishCouncil.Org>
2021-02-01 18:26:24 +0530

    refactored

---
+++ client/src/app/shop/shop.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { IPagination } from '../shared/models/pagination';
+import { IProduct } from '../shared/models/product';
+import { ShopService } from './shop.service';
+
+@Component({
+  selector: 'app-shop',
+  templateUrl: './shop.component.html',
+  styleUrls: ['./shop.component.scss']
+})
+export class ShopComponent implements OnInit {
+
+  products : IProduct[];
+  
+  constructor(private shopService: ShopService ) { }
+
+  ngOnInit() {
+    this.shopService.getProducts()
+    .subscribe((response: IPagination) => {
+      this.products = response.data;
+    },
+    error => {
+      console.log(error)
+    }
+    )
+  }
+
+}
