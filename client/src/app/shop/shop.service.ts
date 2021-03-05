10f81dca0d9540ac9132089d4202545747cd9424
Mrinal Kumar <Mrinal.Kumar@BritishCouncil.Org>
2021-03-05 20:52:39 +0530

    caching at client side part one

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -6,6 +6,7 @@ import {IType} from '../shared/models/productType';
 import {map} from 'rxjs/operators';
 import {ShopParams} from '../shared/models/shopParams';
 import { IProduct } from '../shared/models/product';
+import { of } from 'rxjs';
 
 
 @Injectable({
@@ -14,7 +15,9 @@ import { IProduct } from '../shared/models/product';
 export class ShopService {
 
   private baseUrl: string = 'https://localhost:5001/api/';
-
+  products: IProduct[] = [];
+  brands: IBrand[] = [];
+  types: IType[] = [];
   constructor(private http: HttpClient) { }
 
 
@@ -37,21 +40,45 @@ export class ShopService {
     return this.http.get<IPagination>(this.baseUrl + 'products', {observe: 'response', params})
     .pipe(
            map(response => {
+             this.products = response.body.data; // for clients caching purpose
              return response.body;
            })
     )
   }
 
+  // Here we are checking if product available in inmemory client then get it from there else go to server.
   getProduct(id: number) {
+    const product = this.products.find(p => p.id === id);
+    if (product){
+      return of(product);
+    }
     return this.http.get<IProduct>(this.baseUrl + 'products/' + id);
   }
 
   getBrands() {
-    return this.http.get<IBrand[]>(this.baseUrl + 'products/brands');
+    if (this.brands.length > 0)
+    {
+      return of(this.brands);
+    }
+    return this.http.get<IBrand[]>(this.baseUrl + 'products/brands').pipe(
+      map(response => {
+        this.brands = response; // for client caching purpose we are storing in clinet
+        return response;
+      })
+    )
   }
 
   getTypes() {
-    return this.http.get<IType[]>(this.baseUrl + 'products/types');
+    if (this.types.length > 0)
+    {
+      return of(this.types);
+    }
+    return this.http.get<IType[]>(this.baseUrl + 'products/types').pipe(
+      map(response => {
+        this.types = response;
+        return response;
+      })
+    )
   }
 
 }
