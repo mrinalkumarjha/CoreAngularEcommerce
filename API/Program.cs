2da141a977613fc2c711f4ae56db034254b75aab
Mrinal Jha <mrinalkumarjha@ymail.com>
2024-09-25 15:02:34 +0530

    Api project converted to .net6

--- API/Program.cs
+++ API/Program.cs
@@ -1,26 +1,105 @@
-using System;
-using System.Threading.Tasks;
+using StackExchange.Redis;
 using Core.Entities.Identity;
-using Infrastructure.Data;
-using Infrastructure.Identity;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
-namespace API
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+// add services to container
+
+     // Registering automapper as service.
+        builder.Services.AddAutoMapper(typeof(MappingProfiles)); // mapping profiles is class where we have provided mappings.
+        builder.Services.AddControllers();
+
+        // add dbcontext as service
+        // ordering dosent matter here. ordering matter in middleware section
+
+        builder.Services.AddDbContext<StoreContext>(x => 
+        x.UseSqlServer(builder.Configuration.GetConnectionString("StoreSqlServerConnection")));
+
+        // add identiy db context as service
+        builder.Services.AddDbContext<AppIdentityDbContext>(x => 
+        x.UseSqlServer(builder.Configuration.GetConnectionString("IdentitySqlServerConnection")));
+
+        // Adding redis connnection settiong
+
+        builder.Services.AddSingleton<IConnectionMultiplexer>(c =>
         {
-            //CreateHostBuilder(args).Build().Run(); // prev code
-           var host = CreateHostBuilder(args).Build();
+        var configuration = ConfigurationOptions.Parse(
+        builder.Configuration.GetConnectionString("Redis"), true);
+        configuration.Password =  builder.Configuration.GetConnectionString("RedisPassword");
+        return ConnectionMultiplexer.Connect(configuration);
+        });
+
+        builder.Services.AddApplicationServices(); // coming from extension method.
+        builder.Services.AddIdentityServices(builder.Configuration); // coming from extension
+        builder.Services.AddSwaggerDocumentation(); // coming from swagger extension
+
+
+        // adding CORS
+        //    services.AddCors(opt => {
+        //        opt.AddPolicy("CorsPolicy", policy => {
+        //            policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200");
+        //        });
+        //    });
+
+        builder.Services.AddCors(options =>
+        {
+        options.AddDefaultPolicy(
+        builder =>
+        {
+
+        //you can configure your custom policy
+        builder.AllowAnyOrigin()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+        });
+        });
+
+
+
+        // Configure http request pipeline..
+         // Using custom middleware.
+
+            var app = builder.Build();
+            app.UseMiddleware<ExceptionMiddleware>();
+
+            app.UseStatusCodePagesWithReExecute("/errors/{0}"); // When no endpoint match it will hit this middleware which will redirect to route /errors.
+
+           // app.UseHttpsRedirection(); // this redirect http request to https.
+
+            app.UseRouting(); // Enable to use us routing.
+
+            app.UseStaticFiles(); // Enabling server to serve ststic file. it serve file from wwwroot folder
+
+            // following is to serve image from content folder
+            app.UseStaticFiles(new StaticFileOptions {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(Directory.GetCurrentDirectory(), "Content")
+                ),
+                RequestPath = "/content"
+            });
+
+            //app.UseCors("CorsPolicy");
+             app.UseCors();
+
+            app.UseAuthentication(); // must before UseAuthorization
+
+            app.UseAuthorization();
+
+            app.UseSwaggerDocumentation(); // from extension method.
+
+
+
+            app.MapControllers();
+            // following setting for angular as we are serving angular page from api
+            app.MapFallbackToController("Index", "Fallback");
+        
+
 
            // following code is for creating migration on startup and logging.
-           using(var scope = host.Services.CreateScope())
+           using(var scope = app.Services.CreateScope())
            {
                var services = scope.ServiceProvider;
                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
@@ -47,14 +126,7 @@ namespace API
                }
            }
 
-           host.Run();
-        }
+           await app.RunAsync();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+
+
